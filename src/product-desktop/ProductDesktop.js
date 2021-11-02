80a62f862b750af3759ded3016d45078fed9b304
Simon <cloud9209@gmail.com>
2021-11-02 21:10:07 +0100

    set router

---
+++ src/product-desktop/ProductDesktop.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDesktop = () => {
+  return (
+    <div className="page product-desktop">
+      <h1>ProductDesktop</h1>
+    </div>
+  )
+}
+
+export default ProductDesktop;
---
+++ src/product-mobile/ProductMobile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductMobile = () => {
+  return (
+    <div className="page product-desktop">
+      <h1>ProductDesktop</h1>
+    </div>
+  )
+}
+
+export default ProductMobile;
