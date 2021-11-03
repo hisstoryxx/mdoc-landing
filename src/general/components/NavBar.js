92e11bf7893df1c7574b3379857c963d9339a206
Simon <cloud9209@gmail.com>
2021-11-03 01:14:52 +0100

    add selected status on nav bar menu

--- src/general/components/NavBar.js
+++ src/general/components/NavBar.js
@@ -1,18 +1,28 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import hcLogo from "../img/logo-hc.png";
 import "./NavBar.scss";
 
 const NavBar = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+  const pathNameArray = location.pathname.split("/");
+  const menuName = pathNameArray[pathNameArray.length - 1];
+
   return (
     <div className={"nav-bar"}>
       <div className="boundary">
         <img className="logo" src={hcLogo} alt=""/>
-        <div className="menu-container">
-          <Link to={"/"}>홈</Link>
-          <Link to={"/product/mobile"}>환자 • 보호자</Link>
-          <Link to={"/product/desktop"}>의료진</Link>
-          <Link to={"/contact"}>문의</Link>
+        <div className={`menu-container ${menuName ? menuName : 'home'}`}>
+          <Link className="menu home" to={"/"}>홈</Link>
+          <div className="divider"/>
+          <div className="product-menus">
+            <span className="menu label">제품 소개</span>
+            <Link className="menu mobile" to={"/product/mobile"}>환자 • 보호자</Link>
+            <Link className="menu desktop" to={"/product/desktop"}>의료진</Link>
+          </div>
+          <div className="divider"/>
+          <Link className="menu contact" to={"/contact"}>문의</Link>
         </div>
       </div>
     </div>
