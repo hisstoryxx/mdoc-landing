ffa00a02a5c705fbb960d185ebf154fee2f627f5
Simon <cloud9209@gmail.com>
2021-11-07 10:50:38 +0100

    add contact

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,9 +1,34 @@
 import React from "react";
+import "./Contact.scss";
 
 const Contact = () => {
   return (
     <div className="page contact">
-      <h1>Contact</h1>
+      <div className="contact-intro">
+        <h1>무엇을 도와드릴까요?</h1>
+        <p>문의 사항을 남겨주시면 최대한 빠르게 회신드리겠습니다</p>
+      </div>
+      <div className="contact-form">
+        <div className="form-header">
+          <h1>문의하기</h1>
+          <p>제휴, 제품 사용 등 구금하신 점들을 편하게 문의해주세요</p>
+        </div>
+        <div className="form-body">
+          <div className="input-item">
+            <span>제목</span>
+            <input type="text" placeholder={"제목을 입력해주세요"}/>
+          </div>
+          <div className="input-item">
+            <span>문의 내용</span>
+            <textarea type="text" placeholder={"문의하실 내용을 입력해주세요"}/>
+          </div>
+          <div className="input-item">
+            <span>연락처</span>
+            <input type="text" placeholder={"회신 받을 이메일 혹은 연락처를 입력해주세요"}/>
+          </div>
+          <button className="btn-submit">문의 사항 접수 완료</button>
+        </div>
+      </div>
     </div>
   )
 }
