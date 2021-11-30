1d9b6363214f1ff39cc4cbb1a6e8927c1b8afb81
Simon <cloud9209@gmail.com>
2021-11-30 10:09:41 +0900

    update severance related strings

--- src/home/components/Intro.js
+++ src/home/components/Intro.js
@@ -4,16 +4,23 @@ import mainLogo from "../../general/img/main.png";
 import "./Intro.scss";
 
 const Intro = () => {
-  const title = "안전하고 편리한\n 비대면 재택의료 플랫폼"
+  const title = "안전하고 편리한\n 비대면 재택의료"
   return (
     <div className="intro">
       <img className="main-logo" src={mainLogo} alt="main illustration"/>
       <div className="title-wrapper">
+        <span>엠닥홈케어와 함께하는</span>
         <h1>{title}</h1>
       </div>
       <div className="partner">
-        <span>공동연구개발</span>
-        <span>세브란스병원</span>
+        <div className="severance">
+          <span>공동연구개발</span>
+          <span>세브란스병원</span>
+        </div>
+        <div className="description">
+          <p>엠닥홈케어는 세브란스병원과 공동으로 개발한 재택의료 통합 플랫폼으로,</p>
+          <p>임상적 근거 확보를 위한 연구를 수행하고 있습니다</p>
+        </div>
         {/*<img src={severance} alt=""/>*/}
       </div>
     </div>
