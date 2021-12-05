aa34dd2a6fcb93aa17b1f6d8bc47bda43a302da2
Simon <cloud9209@gmail.com>
2021-12-05 20:09:53 +0900

    hmm

--- src/home/components/Intro.js
+++ src/home/components/Intro.js
@@ -13,13 +13,13 @@ const Intro = () => {
         <h1>{title}</h1>
       </div>
       <div className="partner">
-        <div className="severance">
-          <span>공동연구개발</span>
-          <span>세브란스병원</span>
-        </div>
+        {/*<div className="severance">*/}
+        {/*  <span>공동연구개발</span>*/}
+        {/*  <span>세브란스병원</span>*/}
+        {/*</div>*/}
         <div className="description">
-          <p>엠닥홈케어는 세브란스병원과 공동으로 개발한 재택의료 통합 플랫폼으로,</p>
-          <p>임상적 근거 확보를 위한 연구를 수행하고 있습니다</p>
+          <p>엠닥홈케어는 <strong>연세대학교 의과대학 연구진</strong>과 공동으로 개발한 재택의료 통합 플랫폼으로,</p>
+          <p>임상적 근거 확보를 위한 연구를 수행 중에 있습니다</p>
         </div>
         {/*<img src={severance} alt=""/>*/}
       </div>
