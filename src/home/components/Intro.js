f0182c733273f9e86db599586a48f4a1c775572f
Simon <cloud9209@gmail.com>
2021-12-05 20:30:36 +0900

    update main phrase

--- src/home/components/Intro.js
+++ src/home/components/Intro.js
@@ -18,8 +18,11 @@ const Intro = () => {
         {/*  <span>세브란스병원</span>*/}
         {/*</div>*/}
         <div className="description">
-          <p>엠닥홈케어는 <strong>연세대학교 의과대학 연구진</strong>과 공동으로 개발한 재택의료 통합 플랫폼으로,</p>
-          <p>임상적 근거 확보를 위한 연구를 수행 중에 있습니다</p>
+          <div className="first-line">
+            <span>엠닥홈케어는 <strong>연세대학교 의과대학 연구진</strong>과</span>
+            <span>공동으로 개발한 재택의료 통합 플랫폼으로,</span>
+          </div>
+          <span>임상적 근거 확보를 위한 연구를 수행 중에 있습니다</span>
         </div>
         {/*<img src={severance} alt=""/>*/}
       </div>
