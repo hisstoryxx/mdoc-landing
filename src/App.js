676e14a394b77162aecb655d6087db2f9cd8360d
hisstoryxx <hisstoryxx@gmail.com>
2022-01-22 23:44:37 +0900

    spa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {Switch, Route, useHistory} from "react-router-dom";
+import {Switch, Route, useHistory, Router} from "react-router-dom";
 import NavBar from "./general/components/NavBar";
 import Home from "./home/Home";
 import Contact from "./contact/Contact";
@@ -22,6 +22,7 @@ function App() {
   }, []);
 
   return (
+  
     <div className={"app"}>
       <NavBar/>
 
@@ -30,13 +31,14 @@ function App() {
         <Route path={"/product/mobile"} component={Product}/>
         <Route path={"/product/desktop"} component={Product}/>
         <Route path={"/contact"} component={Contact}/>
-        <Route path={"/check/user"} component={Check}/>
-        <Route path={"/check/data"} component={Check}/>
-        <Route path={"/check/agree"} component={Check}/>
+        <Route exact path={"/check/user"} component={Check}/>
+        <Route exact path={"/check/data"} component={Check}/>
+        <Route exact path={"/check/agree"} component={Check}/>
       </Switch>
       <Footer/>
     
     </div>
+
   );
 }
 
