c8996f474a17d25d6b00985b87e55e9c9a7c68c1
cho <hisstoryxx@gmail.com>
2023-04-20 15:48:15 +0900

    문의하기 emailjs connect

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,11 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
+import emailjs from '@emailjs/browser';
 import "./Contact.scss";
 
 const Contact = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [userContact, setUserContact] = useState("");
+  const [form, setForm] = useState({
+    title: " ",
+    description: " ",
+    userContact: " ",
+  })
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+  // const [userContact, setUserContact] = useState("");
 
+  const serviceID = "service_o2fa0rh";
+  const templateID = "template_1bsoxza";
+  const publicKey = "1It9RYOqQ3DypEBuM";
+
+  const sendEmail = (e) => {
+    //need to send a inquiry mail with title, description, userContact
+    window.alert(`접수되었습니다`);
+    setForm("");
+    // setTitle("");
+    // setDescription("");
+    // setUserContact("");
+
+    e.preventDefault();
+
+    emailjs.send("service_o2fa0rh", "template_1bsoxza", form, publicKey)
+        .then((result) => {
+          console.log(result.text, result.status);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+  }
   return (
     <div className="page contact">
       <div className="contact-intro">
@@ -21,32 +49,33 @@ const Contact = () => {
           <div className="input-item">
             <span>제목</span>
             <input type="text" placeholder={"제목을 입력해주세요"}
-                   value={title}
-                   onChange={(e) => {setTitle(e.target.value)}}/>
+                   value={form.title}
+                   onChange={(e) => {
+                     setForm({...form, title : e.target.value})
+                     }}/>
           </div>
           <div className="input-item">
             <span>문의 내용</span>
             <textarea type="text" placeholder={"문의하실 내용을 입력해주세요"}
-                      value={description}
-                      onChange={(e) => {setDescription(e.target.value)}}/>
+                      value={form.description}
+                      onChange={(e) => {
+                        setForm({...form, description : e.target.value})
+                      }}/>
           </div>
           <div className="input-item">
             <span>연락처</span>
             <input type="text" placeholder={"회신 받을 이메일 혹은 연락처를 입력해주세요"}
-                   value={userContact}
-                   onChange={(e) => {setUserContact(e.target.value)}}/>
+                   value={form.userContact}
+                   onChange={(e) => {
+                     setForm({...form, userContact : e.target.value})
+                     }}/>
           </div>
           <button className="btn-submit"
-                  disabled={!title || !description || !userContact}
-                  onClick={() => {
-                    //need to send a inquiry mail with title, description, userContact
-                    window.alert(`접수되었습니다`);
-                    setTitle("")
-                    setDescription("")
-                    setUserContact("")
-                  }}>문의 사항 접수 완료</button>
+                  disabled={!form.title || !form.description || !form.userContact}
+                  onClick={sendEmail}>문의 사항 접수 완료</button>
         </div>
       </div>
+      
     </div>
   )
 }
